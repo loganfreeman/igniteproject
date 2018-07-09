1ec899b9166ae0875936208578b75d4255583b41
loganfreeman <shanhongcheng@hotmail.com>
2018-07-09 11:31:46 -0600

    google map

--- android/app/src/main/java/com/loganfreeman/hikerguide/MainApplication.java
+++ android/app/src/main/java/com/loganfreeman/hikerguide/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.airbnb.android.react.maps.MapsPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -26,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactNativeI18n(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new MapsPackage()
       );
     }
 
