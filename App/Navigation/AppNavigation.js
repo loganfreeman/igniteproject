6d21f40702f6fe26137d05879feb4e80eeb9779b
loganfreeman <shanhongcheng@hotmail.com>
2018-07-12 13:54:25 -0600

    nested navigator

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,14 +1,55 @@
-import { StackNavigator } from 'react-navigation'
+import React from 'react'
+
 import LaunchScreen from '../Containers/LaunchScreen'
+import { ScrollView, Text, StyleSheet, Button } from 'react-native'
+import { connect } from 'react-redux'
+import { StackNavigator, TabNavigator } from 'react-navigation'
 
 import styles from './Styles/NavigationStyles'
 
 import Routes from '../Cars/config/routes'
+
+// This function is used to create components.
+// It expects a name and a list of optional links to other routes.
+// The generated component is connected to Redux to display the content of the store.
+// This function is used below to easily generate components.
+const createComponent = (name, navigateTo = []) => {
+  return connect(state => ({ redux: state }))(({ navigation, redux }) => (
+    <ScrollView style={{ flex: 1, flexDirection: 'column' }}>
+      {navigateTo.map(route => (
+        <Button
+          key={route}
+          title={`Go to ${route}`}
+          onPress={() => navigation.navigate(route)}
+        />
+      ))}
+    </ScrollView>
+  ))
+}
+
+// The nested StackNavigator
+const NestedNavigator = StackNavigator(
+  {
+    StackOne: { screen: createComponent('StackOne', ['StackTwo']) },
+    StackTwo: { screen: createComponent('StackTwo', ['StackOne']) },
+  },
+  {
+    headerMode: 'none',
+  }
+)
+
 const AppNavigator = StackNavigator(Routes, {
   navigationOptions: ({ navigation }) => {
     return {
       title: navigation.state.params && `${navigation.state.params.title}`,
     }
+  },
+})
+
+// Two tabs, second one is a nested StackNavigator
+export const MainNavigator = TabNavigator({
+  Cars: {
+    screen: AppNavigator,
   },
 })
 
@@ -27,4 +68,4 @@ const PrimaryNav = StackNavigator(
   }
 )
 
-export default AppNavigator
+export default MainNavigator
--- App/Navigation/Styles/NavigationStyles.js
+++ App/Navigation/Styles/NavigationStyles.js
@@ -3,6 +3,14 @@ import { Colors } from '../../Themes/'
 
 export default StyleSheet.create({
   header: {
-    backgroundColor: Colors.backgroundColor
-  }
+    backgroundColor: Colors.backgroundColor,
+  },
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  title: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    padding: 16,
+  },
 })
